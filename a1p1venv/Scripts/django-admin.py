0afbf6d9c05735292bf75a81110dbdeee21c0a03
shashankpatibandla <spatibandla@unomaha.edu>
2018-07-04 11:52:47 -0500

    a1p1 push

---
+++ a1p1venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shashank\documents\usa\isqa 8380 - mdc\assignment1p1\mdc\assignment1p1\a1p1venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
